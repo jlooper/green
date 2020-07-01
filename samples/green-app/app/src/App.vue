844952302afecba4d61afeea72f5ce8e16c64a2e
Jen Looper <jen.looper@gmail.com>
2020-07-01 12:52:36 -0400

    some gradients

--- samples/green-app/app/src/App.vue
+++ samples/green-app/app/src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
-    <h1>Track your Region's Carbon Usage</h1>
-    <h2>hey</h2>
+  <div id="app" :style="background">
+    <h1>Current Carbon Intensity: {{carbonIntensity}}</h1>p
   </div>
 </template>
 
@@ -11,21 +10,34 @@ export default {
   name: "App",
   data() {
     return {
-      fossilFuelPercentage: "",
-      carbonIntensity: "",
-      response: {}
+      response: {},
+      carbonIntensity: 0,
+      background: ""
     };
   },
   async created() {
-    await axios
-      .get(
+    try {
+      const response = await axios.get(
         "https://getcarbonusage.azurewebsites.net/api/getCarbonUsage?region=US-NEISO"
-      )
-      .then(function(response) {
-        console.log(response.data);
-        //this.fossilFuelPercentage = res.fossilFuelPercentage;
-        //this.carbonIntensity = res.carbonIntensity;
-      });
+      );
+      this.response = response.data;
+      this.carbonIntensity = response.data.carbonIntensity;
+      if (this.carbonIntensity <= 100) {
+        this.background =
+          "background: rgb(0,3,69); background: linear-gradient(0deg, rgba(0,3,69,1) 0%, rgba(99,255,3,1) 100%)";
+      } else if (this.carbonIntensity <= 200) {
+        this.background =
+          "background: rgb(0,3,69); background: linear-gradient(0deg, rgba(0,3,69,1) 0%, rgba(255,250,3,1) 100%)";
+      } else if (this.carbonIntensity <= 300) {
+        this.background =
+          "background: rgb(0,3,69); background: linear-gradient(0deg, rgba(0,3,69,1) 0%, rgba(255,139,0,1) 100%)";
+      } else if (this.carbonIntensity > 300) {
+        this.background =
+          "background: rgb(0,3,69); background: linear-gradient(0deg, rgba(0,3,69,1) 0%, rgba(255,0,0,1) 100%)";
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
 </script>
@@ -37,6 +49,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+html,
+body,
+div,
+h1,
+h2 {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+  height: 100%;
+}
+h1 {
+  padding-top: 40px;
+  font-size: 3em;
 }
 </style>
