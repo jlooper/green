121d2074b5e809ae0b546ba0469815c4a2dcd88c
Jen Looper <jen.looper@gmail.com>
2020-06-30 21:20:33 -0400

    new path for API call

--- samples/green-app/app/src/App.vue
+++ samples/green-app/app/src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <div id="app">
-    <h1>Track your Region's Carbon Usage</h1>
-  </div>
+	<div id="app">
+		<h1>Track your Region's Carbon Usage</h1>
+	</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: "App"
+	name: 'App',
+	created() {
+		axios
+			.get('https://api.co2signal.com/v1/latest', {
+				params: {
+					countryCode: 'US-NEISO',
+					headers: {
+						'auth-token': 'c099158761a3a563',
+					},
+				},
+			})
+			.then(function(response) {
+				console.log(response);
+			});
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
