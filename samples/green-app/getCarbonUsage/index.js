d635781b3ca6000268c6e368017d27c8e7a278e1
Jen Looper <jen.looper@gmail.com>
2020-06-30 23:57:15 -0400

    edits for call to Axios

--- samples/green-app/getCarbonUsage/index.js
+++ samples/green-app/getCarbonUsage/index.js
@@ -2,21 +2,22 @@ module.exports = async function (context, req) {
 	const axios = require('axios');
 	const region = req.query.region || (req.body && req.body.region);
 	console.log(region);
+
 	var config = {
 		method: 'get',
-		url: 'https://api.co2signal.com/v1/latest?countryCode=US-NEISO',
+		url: 'https://api.co2signal.com/v1/latest?countryCode=' + region + '',
 		headers: {
 			'auth-token': 'c099158761a3a563',
 		},
 	};
 
-	axios(config)
-		.then(function (response) {
-			context.res = { body: response.data };
-			context.done();
-		})
-		.catch(function (error) {
-			context.res = { body: response.data };
-			context.done();
-		});
+	try {
+		const { data } = await axios(config);
+		context.log(data);
+		// do something with the data
+		return data;
+	} catch (err) {
+		context.log(err);
+		// do something with the error
+	}
 };
