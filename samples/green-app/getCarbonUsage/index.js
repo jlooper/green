949fad07e8080469b1aee2eb76eaff729f2216ce
Jen Looper <jen.looper@gmail.com>
2020-06-30 22:37:16 -0400

    edits for function

--- samples/green-app/getCarbonUsage/index.js
+++ samples/green-app/getCarbonUsage/index.js
@@ -2,24 +2,21 @@ module.exports = async function (context, req) {
 	const axios = require('axios');
 	const region = req.query.region || (req.body && req.body.region);
 	console.log(region);
-	axios
-		.get('https://api.co2signal.com/v1/latest', {
-			params: {
-				countryCode: region,
-				headers: {
-					'auth-token': 'c099158761a3a563',
-				},
-			},
-		})
+	var config = {
+		method: 'get',
+		url: 'https://api.co2signal.com/v1/latest?countryCode=US-NEISO',
+		headers: {
+			'auth-token': 'c099158761a3a563',
+		},
+	};
+
+	axios(config)
 		.then(function (response) {
-			console.log(response);
-			if (error == null) {
-				context.res = { body: response.data };
-				context.done();
-			}
+			context.res = { body: response.data };
+			context.done();
 		})
 		.catch(function (error) {
-			context.res = { body: error };
+			context.res = { body: response.data };
 			context.done();
 		});
 };
