d81538ac8002077d662fc774f28e28f7059a1bfe
Jen Looper <jen.looper@gmail.com>
2020-06-30 22:19:53 -0400

    using function

--- samples/green-app/app/src/App.vue
+++ samples/green-app/app/src/App.vue
@@ -1,28 +1,32 @@
 <template>
-	<div id="app">
-		<h1>Track your Region's Carbon Usage</h1>
-	</div>
+  <div id="app">
+    <h1>Track your Region's Carbon Usage</h1>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-	name: 'App',
-	created() {
-		axios.get('/api/getCarbonUsage?region=US-NEISO').then(function(response) {
-			console.log(response);
-		});
-	},
+  name: "App",
+  created() {
+    axios
+      .get(
+        "https://getcarbonusage.azurewebsites.net/api/getCarbonUsage?region=US-NEISO"
+      )
+      .then(function(response) {
+        console.log(response);
+      });
+  }
 };
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
