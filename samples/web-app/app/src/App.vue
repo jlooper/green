a1f756df59c64f34bfc29aad1127a3864dc43efa
Jen Looper <jen.looper@gmail.com>
2020-07-07 11:40:24 -0400

    typo fix

--- samples/web-app/app/src/App.vue
+++ samples/web-app/app/src/App.vue
@@ -1,58 +1,58 @@
 <template>
-  <div id="app" :style="background">
-    <h1>Current Carbon Intensity: {{carbonIntensity}}</h1>p
-  </div>
+	<div id="app" :style="background">
+		<h1>Current Carbon Intensity: {{ carbonIntensity }}</h1>
+	</div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 export default {
-  name: "App",
-  data() {
-    return {
-      response: {},
-      carbonIntensity: "calculating...",
-      background: ""
-    };
-  },
-  async created() {
-    try {
-      const response = await axios.get(
-        "https://getcarbonusage.azurewebsites.net/api/getCarbonUsage?region=US-NEISO"
-      );
-      this.response = response.data;
-      this.carbonIntensity = response.data.carbonIntensity.toFixed(2);
-      if (this.carbonIntensity <= 100) {
-        //green
-        this.background =
-          "background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(151,222,107,1) 100%);";
-      } else if (this.carbonIntensity <= 200) {
-        //yellow
-        this.background =
-          "background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(236,214,98,1) 100%);";
-      } else if (this.carbonIntensity <= 300) {
-        //orange
-        this.background =
-          "background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(242,145,65,1) 100%);";
-      } else if (this.carbonIntensity > 300) {
-        //red
-        this.background =
-          "background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(242,85,65,1) 100%);";
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+	name: 'App',
+	data() {
+		return {
+			response: {},
+			carbonIntensity: 'calculating...',
+			background: '',
+		};
+	},
+	async created() {
+		try {
+			const response = await axios.get(
+				'https://getcarbonusage.azurewebsites.net/api/getCarbonUsage?region=US-NEISO'
+			);
+			this.response = response.data;
+			this.carbonIntensity = response.data.carbonIntensity.toFixed(2);
+			if (this.carbonIntensity <= 100) {
+				//green
+				this.background =
+					'background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(151,222,107,1) 100%);';
+			} else if (this.carbonIntensity <= 200) {
+				//yellow
+				this.background =
+					'background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(236,214,98,1) 100%);';
+			} else if (this.carbonIntensity <= 300) {
+				//orange
+				this.background =
+					'background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(242,145,65,1) 100%);';
+			} else if (this.carbonIntensity > 300) {
+				//red
+				this.background =
+					'background: rgb(255,255,255); background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(242,85,65,1) 100%);';
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 html,
@@ -60,16 +60,16 @@ body,
 div,
 h1,
 h2 {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-  height: 100%;
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+	height: 100%;
 }
 h1 {
-  padding-top: 40px;
-  font-size: 3em;
+	padding-top: 40px;
+	font-size: 3em;
 }
 </style>
